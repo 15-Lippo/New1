8251c42fda9dad89d928f13b845572cf6756d270
francesco lippo <lippo.f80@gmail.com>
2025-04-05 14:51:39 +0200

    Fix: Add buffer and other polyfills, update build configuration

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -10,7 +10,14 @@ const nextConfig = {
   swcMinify: true,
   transpilePackages: ['@gamba/core', '@gamba/react', '@gamba/react-ui'],
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false,
+      buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+    }
     return config
   }
 }
